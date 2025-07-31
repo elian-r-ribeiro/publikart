7f6c0b3b0c750b10330b974dd6e24e6a26dcc502
Elian <elianrodriguesribeiro@gmail.com>
2025-07-31 11:34:56 -0300

    Fixed a typo

--- src/components/others/cards/MiniPlaylistCard.tsx
+++ src/components/others/cards/MiniPlaylistCard.tsx
@@ -9,7 +9,7 @@ interface PlaylistCardProps {
 export default function MiniPlaylistCard(props: PlaylistCardProps) {
     return (
         <div className="min-w-32 bg-zinc-700/40 rounded-lg p-4 backdrop-blur changeScaleOnHoverDefaultStyle">
-            <img src={props.playlist.image} alt="Album image" className="w-full h-32 object-cover rounded-lg mb-2" />
+            <img src={props.playlist.image} alt="Playlist image" className="w-full h-32 object-cover rounded-lg mb-2" />
             <div className="text-center">
                 <h2 className="text-lg font-semibold truncate overflow-hidden whitespace-nowrap">{props.playlist.name}</h2>
             </div>
