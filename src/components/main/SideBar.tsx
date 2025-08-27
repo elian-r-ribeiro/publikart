9a83afbce4a808b61ded8115879db4b99c5f6173
Elian <elianrodriguesribeiro@gmail.com>
2025-08-27 11:13:50 -0300

    Changed side bar order, removed explore page and added my songs and saved songs pages

--- src/components/main/SideBar.tsx
+++ src/components/main/SideBar.tsx
@@ -1,4 +1,4 @@
-import { IconAlbum, IconAlbumOff, IconBook, IconFolder, IconMusic, IconPlaylist, IconPlaylistAdd, IconPlaylistX, IconRadio, IconUsersGroup } from "@tabler/icons-react";
+import { IconAlbum, IconAlbumOff, IconBook, IconMusic, IconMusicCheck, IconMusicX, IconPlaylist, IconPlaylistAdd, IconPlaylistX, IconRadio, IconUsersGroup } from "@tabler/icons-react";
 import SideBarItem from "./SideBarItem";
 import SideBarLabel from "./SideBarLabel";
 import MiniUserProfile from "../mini/MiniUserProfile";
@@ -7,17 +7,17 @@ export default function SideBar() {
     return (
         <nav className="hidden w-80 lg:flex flex-col gap-5 my-4 mx-4">
             <MiniUserProfile />
-            <SideBarLabel label="Para você" />
-            <SideBarItem link="/explore" iconName={IconFolder} text="Explorar" />
+            <SideBarLabel label="Explorar" />
             <SideBarItem link="/songs" iconName={IconMusic} text="Músicas" />
             <SideBarItem link="/artists" iconName={IconUsersGroup} text="Compositores" />
-            <SideBarLabel label="Playlists" />
             <SideBarItem link="/publicPlaylists" iconName={IconPlaylist} text="Playlists Públicas" />
+            <SideBarItem link="/publicAlbums" iconName={IconBook} text="Álbuns Públicos" />
+            <SideBarLabel label="Seus" />
+            <SideBarItem link="/mySongs" iconName={IconMusicX} text="Suas Músicas" />
+            <SideBarItem link="/savedSongs" iconName={IconMusicCheck} text="Músicas Salvas" />
             <SideBarItem link="/savedPlaylists" iconName={IconPlaylistAdd} text="Playlists Salvas" />
             <SideBarItem link="/myPlaylists" iconName={IconPlaylistX} text="Minhas Playlists" />
-            <SideBarLabel label="Álbuns" />
-            <SideBarItem link="/publicAlbums" iconName={IconAlbum} text="Álbuns Públicos" />
-            <SideBarItem link="/savedAlbums" iconName={IconBook} text="Álbuns Salvos" />
+            <SideBarItem link="/savedAlbums" iconName={IconAlbum} text="Álbuns Salvos" />
             <SideBarItem link="/myAlbums" iconName={IconAlbumOff} text="Meus Álbuns" />
             <SideBarLabel label="Outros" />
             <SideBarItem link="radio" iconName={IconRadio} text="Rádio" />
