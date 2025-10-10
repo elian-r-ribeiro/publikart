814c43215f0e0560f3f70d3bfe6b6374bc1b8cdd
Lobo <elianrodriguesribeiro@gmail.com>
2025-10-10 15:54:42 -0300

    Fixed artist page image width and changed application icon

--- src/components/cards/ArtistCard.tsx
+++ src/components/cards/ArtistCard.tsx
@@ -10,7 +10,7 @@ export default function ArtistCard(props: ArtistCardProps) {
 
     return (
         <div className="centerItemsRow gap-6 bg-zinc-700/40 backdrop-blur rounded-lg p-4 sm:w-64 md:w-128">
-            <img className="w-24 h-24 rounded-full" src={props.artist.profilePictureURL} alt="Artist image" />
+            <img className="w-24 h-24 rounded-full object-cover" src={props.artist.profilePictureURL} alt="Artist image" />
             <p>{props.artist.userName}</p>
         </div>
     );
